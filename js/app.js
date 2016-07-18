dd51717f843d835c4a6f81f3f51daa04abecd884
ChristopherSO <androidcaso11@gmail.com>
2016-07-17 22:47:52 -0600

    Lista conteo de veces en que aparece cada miembro

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@
 		for (var i=0; i<membersData.length; i++) {
 			this.members.push(Member.build(membersData[i]));
 		}
+		console.log(Member.appearances);
 
 		this.monthNames = monthNames;
 		this.weeks;
@@ -46,43 +47,6 @@
 
 		});
 	});
-
-	app.factory('Member', function () {
-
-		//Constructor
-		function Member(name, isDirector, isMusician, isChorister, isSoundman) {
-			// Public properties, assigned to the instance ('this')
-			this.name = name;
-			this.isDirector = isDirector;
-			this.isMusician = isMusician;
-			this.isChorister = isChorister;
-			this.isSoundman = isSoundman;
-		}
-
-
-		/**
-		* Static method, assigned to class
-		* Instance ('this') is not available in static context
-		*/
-		Member.build = function (data) {
-
-			return new Member(
-				data.name,
-				data.isDirector,
-				data.isMusician,
-				data.isChorister,
-				data.isSoundman
-			);
-		};
-
-
-		/**
-		* Return the constructor function
-		*/
-		return Member;
-	});
-
-
 
 	var weeks = [];
 
--- js/models/Members.js
+++ js/models/Members.js
@@ -2,40 +2,45 @@
 
 var app = angular.module('rol-members', []);
 
-app.factory('Member', function () {
+	app.factory('Member', function () {
 
-	//Constructor
-	function Member(name, isDirector, isMusician, isChorister, isSoundman) {
-		// Public properties, assigned to the instance ('this')
-		this.name = name;
-		this.isDirector = isDirector;
-		this.isMusician = isMusician;
-		this.isChorister = isChorister;
-		this.isSoundman = isSoundman;
-	}
+		//Constructor
+		function Member(name, isDirector, isMusician, isChorister, isSoundman) {
+			// Public properties, assigned to the instance ('this')
+			this.name = name;
+			this.isDirector = isDirector;
+			this.isMusician = isMusician;
+			this.isChorister = isChorister;
+			this.isSoundman = isSoundman;
+		}
+
+		// Number of times the member appears in the rol
+		Member.appearances = {};
+
+		/**
+		* Static method, assigned to class
+		* Instance ('this') is not available in static context
+		*/
+		Member.build = function (data) {
+			
+			// Initialize member appearances
+			Member.appearances[data.name] = 0;
+
+			return new Member(
+				data.name,
+				data.isDirector,
+				data.isMusician,
+				data.isChorister,
+				data.isSoundman
+			);
+		};
 
 
-	/**
-	* Static method, assigned to class
-	* Instance ('this') is not available in static context
-	*/
-	Member.build = function (data) {
-
-		return new Member(
-			data.name,
-			data.isDirector,
-			data.isMusician,
-			data.isChorister,
-			data.isSoundman
-		);
-	};
-
-
-	/**
-	* Return the constructor function
-	*/
-	return Member;
-});
+		/**
+		* Return the constructor function
+		*/
+		return Member;
+	});
 
 
 })();
